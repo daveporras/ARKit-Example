3693c697100197990f6f4c35aced9d261c9f38f6
Bockdan David Porras <bockdandavid.porras@externos-cl.cencosud.com>
2019-04-30 17:53:34 -0400

    improvements

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { AR, Asset } from 'expo';
-import ExpoTHREE, { AR as ThreeAR, THREE } from 'expo-three';
-import { View as GraphicsView } from 'expo-graphics';
+import { AR } from 'expo';
+import { GraphicsView } from 'expo-graphics';
+import ExpoTHREE, { THREE, AR as ThreeAR } from 'expo-three';
+
+const screenCenter = new THREE.Vector2(0.5, 0.5);
 
 export default class App extends React.Component {
-  componentDidMount() {
-    THREE.suppressExpoWarnings(true)
-    ThreeAR.suppressWarnings()
+  componentWillMount() {
+    THREE.suppressExpoWarnings();
   }
 
   // Cuando el contexto es creado, se puede empezar a crear cosas en 3D.
-  onContextCreate = async ({ gl, scale: pixelRatio, width, height }) => {
+  onContextCreate = ({ gl, width, height, scale }) => {
     // Esto permite a AR detectar superfices horizontales.
-    AR.setPlaneDetection('horizontal');
+    AR.setPlaneDetection(AR.PlaneDetection.Horizontal);
 
     // Crea un 3D renderer
     this.renderer = new ExpoTHREE.Renderer({
       gl,
-      pixelRatio,
       width,
       height,
+      pixelRatio: scale,
     });
 
     // Nuestra escena.
@@ -29,30 +30,35 @@ export default class App extends React.Component {
     this.scene.background = new ThreeAR.BackgroundTexture(this.renderer);
 
     // Crear una cámara que coincida con la orientación del dispositivo.
-    this.camera = new ThreeAR.Camera(width, height, 0.01, 1000);
-    
+    this.camera = new ThreeAR.Camera(width, height, 0.1, 1000);
+
     // Hacemos un cubo. Cada unidad equivale a 1 metro en la vida real.
     const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+
     // Color del material
-    const material = new THREE.MeshBasicMaterial({
-      color: 0xff00ff,
-    });
-    
+    const material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+
     // Combinamos la geometria con el material.
     this.cube = new THREE.Mesh(geometry, material);
+
     // Ubicamos el cubo a 0.4 metros de distancia de nosotros.
     this.cube.position.z = -0.4
+
+    /* this.magnetic = new ThreeAR.MagneticObject();
+    this.magnetic.add(this.cube);
+    this.scene.add(this.magnetic); */
+
     // Añadimos el cubo a la escena.
     this.scene.add(this.cube);
-    
+
     // Configura el color de la luz para que podamos ver el color del cubo.
-    var light = new THREE.AmbientLight(0xffffff);
-    this.scene.add(light);
+    this.scene.add(new THREE.AmbientLight(0x404040));
+    this.scene.add(new THREE.DirectionalLight(0xffffff, 0.6));
   };
 
-  // Llamado por cada cuadro.
   onRender = () => {
     // Finalmente renderiza la escena con la Camara AR.
+    /* this.magnetic.update(this.camera, screenCenter); */
     this.renderer.render(this.scene, this.camera);
   };
 
@@ -67,7 +73,7 @@ export default class App extends React.Component {
     this.renderer.setPixelRatio(scale);
     this.renderer.setSize(width, height);
   };
-  
+
   render() {
     // Las propiedades `isArEnabled` & `arTrackingConfiguration` son requeridas.
     // `isArRunningStateEnabled` mostrara un botón de play/pause en la ezquina de la pantalla.
@@ -76,14 +82,14 @@ export default class App extends React.Component {
     return (
       <GraphicsView
         style={{ flex: 1 }}
+        isArEnabled
         onContextCreate={this.onContextCreate}
         onRender={this.onRender}
         onResize={this.onResize}
-        isArEnabled
         arTrackingConfiguration={AR.TrackingConfiguration.World}
         isArRunningStateEnabled={true}
         isArCameraStateEnabled={true}
       />
     );
-  } 
-}
+  }
+};
